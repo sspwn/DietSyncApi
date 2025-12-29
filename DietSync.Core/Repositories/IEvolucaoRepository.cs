b97d63ccf10417d513223f536f65d6962f4baae5
segobinho <gabrielsegobi@edu.unifil.br>
2025-12-28 21:52:22 -0300

    EvolucaoRepository

---
+++ DietSync.Core/Repositories/IEvolucaoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DietSync.Core.Entities.Filters;
+using DietSync.Core.Entities;
+
+namespace DietSync.Core.Repositories
+{
+    public interface IEvolucaoRepository
+    {
+        Task<List<Evolucao>> GetAll(EvolucaoFilter filter);
+        Task<Evolucao?> GetById(long id);
+        Task<long> Create(Evolucao evolucao);
+        Task Update(Evolucao evolucao);
+        Task Delete(Evolucao evolucao);
+    }
+}
---
+++ DietSync.Infrastructure/Repositories/EvolucaoRepository.cs
@@ -0,0 +1,48 @@
+﻿using DietSync.Core.Entities;
+using DietSync.Core.Entities.Filters;
+using DietSync.Core.Repositories;
+using DietSync.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DietSync.Infrastructure.Repositories
+{
+    public class EvolucaoRepository : IEvolucaoRepository
+    {
+        private readonly DietSyncDbContext _dbContext;
+
+        public EvolucaoRepository(DietSyncDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<long> Create(Evolucao evolucao)
+        {
+            await _dbContext.Evolucoes.AddAsync(evolucao);
+            return evolucao.Id;
+        }
+
+        public Task Delete(Evolucao evolucao)
+        {
+            _dbContext.Evolucoes.Remove(evolucao);
+            return Task.CompletedTask;
+        }
+
+        public Task<List<Evolucao>> GetAll(EvolucaoFilter filter)
+        {
+            var evolucoes = _dbContext.Evolucoes.ToListAsync();
+            return evolucoes;
+        }
+
+        public Task<Evolucao?> GetById(long id)
+        {
+            var evolucao = _dbContext.Evolucoes.FirstOrDefaultAsync(e => e.Id == id);
+            return evolucao;
+        }
+
+        public Task Update(Evolucao evolucao)
+        {
+            _dbContext.Evolucoes.Update(evolucao);
+            return Task.CompletedTask;
+        }
+    }
+}
