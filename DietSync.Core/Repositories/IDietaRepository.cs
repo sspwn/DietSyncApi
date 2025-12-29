b5b184e5fe49737d60aeafe748fc0691c78d6d13
segobinho <gabrielsegobi@edu.unifil.br>
2025-12-28 21:52:01 -0300

    DietaRepository

---
+++ DietSync.Core/Repositories/IDietaRepository.cs
@@ -0,0 +1,14 @@
+﻿using DietSync.Core.Entities.Filters;
+using DietSync.Core.Entities;
+
+namespace DietSync.Core.Repositories
+{
+    public interface IDietaRepository
+    {
+        Task<List<Dieta>> GetAll(DietaFilter filter);
+        Task<Dieta?> GetById(long id);
+        Task<long> Create(Dieta dieta);
+        Task Update(Dieta dieta);
+        Task Delete(Dieta dieta);
+    }
+}
