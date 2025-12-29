f95fc0a3b7bbbf6aff11d390424300621a47fed1
segobinho <gabrielsegobi@edu.unifil.br>
2025-12-28 21:52:50 -0300

    TreinoRepository

---
+++ DietSync.Infrastructure/Repositories/TreinoRepository.cs
@@ -0,0 +1,48 @@
+﻿using DietSync.Core.Entities;
+using DietSync.Core.Entities.Filters;
+using DietSync.Core.Repositories;
+using DietSync.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DietSync.Infrastructure.Repositories
+{
+    public class TreinoRepository : ITreinoRepository
+    {
+        private readonly DietSyncDbContext _dbContext;
+
+        public TreinoRepository(DietSyncDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<long> Create(Treino treino)
+        {
+            var addtreino = await _dbContext.Treinos.AddAsync(treino);
+            return treino.Id;
+        }
+
+        public Task Delete(Treino treino)
+        {
+            _dbContext.Remove(treino);
+            return Task.CompletedTask;
+        }
+
+        public async Task<List<Treino>> GetAll(TreinoFilter filter)
+        {
+            var treinos = await _dbContext.Treinos.ToListAsync();
+            return treinos;
+        }
+
+        public async Task<Treino?> GetById(long id)
+        {
+            var treino = await _dbContext.Treinos.FirstOrDefaultAsync(t => t.Id == id);
+            return treino;
+        }
+
+        public Task Update(Treino treino)
+        {
+            _dbContext.Update(treino);
+            return Task.CompletedTask;
+        }
+    }
+}
