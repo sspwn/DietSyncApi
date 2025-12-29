056d5422ca4910126dfaeec60d45959e15235990
segobinho <gabrielsegobi@edu.unifil.br>
2025-12-28 21:53:42 -0300

    UnitOfWork

---
+++ DietSync.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using DietSync.Core.Repositories;
+using DietSync.Infrastructure.Persistence;
+
+namespace DietSync.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DietSyncDbContext _context;
+
+        public UnitOfWork(DietSyncDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
